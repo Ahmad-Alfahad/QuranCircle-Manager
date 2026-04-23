2dd2cd4e7157d29582ce27da9bc08f9d06e2fc81
Ahmad-Alfahad <ahmad222alfahad@gmail.com>
2026-04-23 19:47:20 +0300

    install breeze  and tailwindcss

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('index' , [TestController::class, 'index']);
+
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+    ->name('login');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->name('logout');
+
+Route::get('/register', [RegisteredUserController::class, 'create'])
+    ->name('register');
+
+Route::post('/register', [RegisteredUserController::class, 'store']);
